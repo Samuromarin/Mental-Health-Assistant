40ba18f6662e33d36785460f84dada84d10617ed
Samuromarin <samuromarin@gmail.com>
2025-06-21 00:26:41 +0200

    Gemma

--- src/config/settings.py
+++ src/config/settings.py
@@ -35,8 +35,8 @@ os.makedirs(os.path.join(DATA_DIR, "faiss_index"), exist_ok=True)
 # Crisis detection keywords configuration (English)
 CRISIS_KEYWORDS = [
     "suicide", "kill myself", "end my life", "don't want to live", 
-    "self-harm", "cut myself", "hurt myself", "die", "end it all",
-    "no point in living", "nobody would miss me", "better without me",
+    "self-harm", "cut myself", "hurt myself", "die", "end everything",
+    "no point living", "nobody would miss me", "better without me",
     "can't take it anymore", "can't go on", "want to die", "burden", "no way out", 
     "harm myself", "hurt myself", "hopeless", "goodbye forever", "can't resist anymore"
 ]
